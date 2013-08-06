e0bc626e32dbb3c17d53c5a6ad1e5430ec0d872f
zhujf <bgi_zhujf@zhujf.(none)>
2013-08-06 15:05:18 +0800

    cp ../.emacs ./init.el

---
+++ init.el
@@ -0,0 +1,19 @@
+(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+(require 'init-gui-frames)
+(require 'init-utils)
+(require 'init-windows)
+(require 'init-uniquify)
+(require 'init-evil)
+(require 'init-editing-utils)
+
+(require 'init-recentf)
+(require 'init-etags)
+(require 'init-ido)
+(require 'init-hippie-expand)
+(require 'init-yasnippet)
+(require 'init-auto-complete)
+
+(require 'init-C)
+(require 'init-R)

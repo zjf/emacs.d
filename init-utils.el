f8fa32ae9dfbaf47db072dfcca33b706c0699379
Jianfeng Zhu <eric.zjf@gmail.com>
2015-02-08 12:03:20 +0800

    Update init-utils.el
    sync exec-path with $PATH

--- init-utils.el
+++ init-utils.el
@@ -29,4 +29,14 @@
         (set-visited-file-name new-name)
         (set-buffer-modified-p nil)))))
 
+;; For GUI emacs under OS X
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
 (provide 'init-utils)

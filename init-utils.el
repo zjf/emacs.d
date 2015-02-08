ec7cc1890128066d619b9f3bf8b73f3424174360
Jianfeng Zhu <eric.zjf@gmail.com>
2015-02-08 12:10:17 +0800

    Update init-utils.el
    auto set exec-path for os

--- init-utils.el
+++ init-utils.el
@@ -31,12 +31,11 @@
 
 ;; For GUI emacs under OS X
 (defun set-exec-path-from-shell-PATH ()
-  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-
-This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell. This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
   (interactive)
   (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
+(when (equal system-type 'darwin) (set-exec-path-from-shell-PATH))
 
 (provide 'init-utils)

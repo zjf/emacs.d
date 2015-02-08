d7cd6f91da5aca439a4be7389964c799139dbf4d
Jianfeng Zhu <eric.zjf@gmail.com>
2015-02-08 12:11:54 +0800

    Update init-editing-utils.el
    add pbcopy, pbpaste, pbcut for gui emacs

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -29,6 +29,24 @@
  x-select-enable-clipboard t
  visible-bell nil)
 
+(defun pbcopy ()
+  (interactive)
+    (call-process-region (point) (mark) "pbcopy")
+      (setq deactivate-mark t))
+
+(defun pbpaste ()
+  (interactive)
+    (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
+
+(defun pbcut ()
+  (interactive)
+    (pbcopy)
+      (delete-region (region-beginning) (region-end)))
+
+(global-set-key (kbd "C-c k") 'pbcopy)
+(global-set-key (kbd "C-c v") 'pbpaste)
+(global-set-key (kbd "C-c x") 'pbcut)
+
 ;; smooth scrolling
 ;; http://stackoverflow.com/questions/3631220/fix-to-get-smooth-scrolling-in-emacs
 (setq redisplay-dont-pause t

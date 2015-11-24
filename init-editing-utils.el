d99c6a304fffb5632f57c6f673816cbe3d2dc284
zhujianfeng <eric.zjf@gmail.com>
2015-11-24 23:07:56 +0800

    add utf-8 config

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -1,6 +1,16 @@
 (require 'diminish)
 (require 'whole-line-or-region)
 
+;; terminal = UTF-8 settings
+(prefer-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(modify-coding-system-alist 'process "*" 'utf-8)
+
 (when (fboundp 'electric-pair-mode)
   (setq-default electric-pair-mode 1))
 
